2d00d18d23b68f01f41fc341d1cfaae11437e69c
Aditya <adityakatyal12345@gmail.com>
2026-06-11 15:31:24 +0530

    Added new concepts

--- script.js
+++ script.js
@@ -1,76 +1,109 @@
-// console.log("Hi!") //Print statement
-// console.log(5+5)
+// // console.log("Hi!") //Print statement
+// // console.log(5+5)
 
 
-//Variables
-//Variables in JavaScript are the storage containers to store specific values. 
+// //Variables
+// //Variables in JavaScript are the storage containers to store specific values. 
 
-//Defining varaibles in JS: 
-//Mutable, i.e. Can be changed.    //Immutable, i.e Can't be changed. 
+// //Defining varaibles in JS: 
+// //Mutable, i.e. Can be changed.    //Immutable, i.e Can't be changed. 
 
-//Loops in JS
-//Loops in JS are used to run a specific task n no. of times.
+// //Loops in JS
+// //Loops in JS are used to run a specific task n no. of times.
 
-//Types: 1. for Loop
-for(let i = 1; i <= 50; i++){
-    console.log(i)
-}
+// //Types: 1. for Loop
+// for(let i = 1; i <= 50; i++){
+//     console.log(i)
+// }
 
-//2. while loop
-let toffee = 10;
-while (toffee > 0){     //toffee = 10 => 10 > 0
-    console.log(toffee)     //Print toffee ki value
-    toffee --;      //toffee ki value mein se ek minus krdia => toffee = 10 - 1 = 9
-}
+// //2. while loop
+// let toffee = 10;
+// while (toffee > 0){     //toffee = 10 => 10 > 0
+//     console.log(toffee)     //Print toffee ki value
+//     toffee --;      //toffee ki value mein se ek minus krdia => toffee = 10 - 1 = 9
+// }
 
-//3. do-while  
-// do {
-//    console.log("Hello") 
-//    a--;
-// } while(a > 0);
-
-
-//if-else
-const temperature = 15;
-if(temperature <= 40){
-    console.log('Set ac to 20');
-} else if (temperature <= 30){
-    console.log('Set ac to 25');
-} else{
-    console.log('AC off')
-}
+// //3. do-while  
+// // do {
+// //    console.log("Hello") 
+// //    a--;
+// // } while(a > 0);
 
 
-const a = 5;    //Integer
-const b = 5.0;  //Float
-const c = "5";  //String
+// //if-else
+// const temperature = 15;
+// if(temperature <= 40){
+//     console.log('Set ac to 20');
+// } else if (temperature <= 30){
+//     console.log('Set ac to 25');
+// } else{
+//     console.log('AC off')
+// }
 
-if(a === c){
-    console.log('True')
-}else{
-    console.log('False')
-}
 
-//Operators in JS
-//Types:
+// const a = 5;    //Integer
+// const b = 5.0;  //Float
+// const c = "5";  //String
 
-//1: Arithemetic
+// if(a === c){
+//     console.log('True')
+// }else{
+//     console.log('False')
+// }
 
-// console.log(4+5)    //8
-// console.log(3**2)   //9
+// //Operators in JS
+// //Types:
 
-//2. Assignment Operator
+// //1: Arithemetic
 
-let y = 5;
+// // console.log(4+5)    //8
+// // console.log(3**2)   //9
 
-y = y + 1
-y += 1
-y -= 1
-y /= 1
-y *= 1
+// //2. Assignment Operator
 
-//3. Comparsion Operator
-// ==, <=, >=, ===, != (not equals to), <, >, !==
+// let y = 5;
 
-//4. Logical Operators
-// &&, ||, !
+// y = y + 1
+// y += 1
+// y -= 1
+// y /= 1
+// y *= 1
+
+// //3. Comparsion Operator
+// // ==, <=, >=, ===, != (not equals to), <, >, !==
+
+// //4. Logical Operators
+// // &&, ||, !
+
+// // Type Casting in JS
+// // In JS, changing a value from one data type to another is refrred to as Type Casting. 
+// // Data type of a value describes of how a value is represented. For example: Integer, Float, Strings, etc.
+
+// //Ex: Converting a Number to String, Intger value to Float Value, etc.
+
+// //Types of Type Casting: 1. Explicit Type Casting, 2. Implicit Type Casting
+
+// const num = 123
+// console.log(num)
+// const strNum = String(num)
+
+// if (num === strNum){
+//     console.log('True')
+// }
+// else{
+//     console.log("False")
+// }
+
+
+//Input Methods in JS
+
+// // Method 1: Console Inputs
+// const a = Number(prompt('Laa re chikne de: '))
+// const b = prompt('Laa ek baar aur de: ')
+// console.log(a+b)
+
+//Method 2: HTML Form inputs
+
+// const htmlVal = document.getElementById('test').value
+// console.log(htmlVal)
+
