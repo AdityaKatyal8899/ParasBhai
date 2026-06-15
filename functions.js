34556e61a9ed5ce9d6fa92b5dc1589db3182c493
Aditya <adityakatyal12345@gmail.com>
2026-06-15 23:51:59 +0530

    15/6/26

---
+++ functions.js
@@ -0,0 +1,50 @@
+// const val = Number(prompt("Enter a value:"))
+
+// if(val > 5){
+//  for(let i = 1; i <= 5; i++){
+//     console.log(val*i)
+//     }   
+// }
+
+// else if(val === 5){
+//     for(let i = 1; i <= 7; i++){
+//     console.log(val*i)
+//     }
+// }
+
+// else{
+//     for(let i = 1; i <= 10; i++){
+//     console.log(val*i)
+//     }
+// }
+
+//Functions: Functions in JS are reusable code blocks that can be called and used anywhere in the program. 
+
+// function tableFormer(val){
+//     for(let i = 1; i <= 10; i++){
+//     console.log(val*i)
+//     }
+// }
+
+// function isEven(val){
+//     if (val%2 === 0){
+//         console.log(true)
+//     }
+//     else{
+//         console.log(false)
+//     }
+// } 
+
+// function printer(val){
+//     console.log(typeof(val))
+// }
+
+// const num = Number(prompt("Enter a value:"))
+// printer('true')
+
+//Arguments: Function mein jo cheez hum pass krte h, unko arguments bolte h
+//Parameters: Function jha passed data ko store krta h, unko parameters bolte h
+
+
+
+
